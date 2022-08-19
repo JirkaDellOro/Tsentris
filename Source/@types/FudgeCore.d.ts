20e7d79dd51c71ec4279f8055821cf32bdc5e9b5
Jirka Dell'Oro-Friedl <del@hs-furtwangen.de>
2022-08-19 12:47:34 +0200

    touchMove as custom event including offset

--- Source/@types/FudgeCore.d.ts
+++ Source/@types/FudgeCore.d.ts
@@ -2563,14 +2563,8 @@ declare namespace FudgeCore {
 }
 declare namespace FudgeCore {
     enum EVENT_TOUCH {
-        /** the standard touchstart, in here for completeness */
-        START = "touchstart",
-        /** the standard touchend, in here for completeness */
-        END = "touchend",
-        /** the standard touchmove, in here for completeness */
-        MOVE = "touchmove",
-        /** the standard touchcancel, in here for completeness */
-        CANCEL = "touchcancel",
+        /** custom event fired in addition to the standard touchmove, details offset to starting touch */
+        MOVE = "touchMove",
         /** custom event fired when the touches haven't moved outside of the tap radius */
         TAP = "touchTap",
         /** custom event fired when the touches have moved outside of the notch radius, details offset and cardinal direction */
